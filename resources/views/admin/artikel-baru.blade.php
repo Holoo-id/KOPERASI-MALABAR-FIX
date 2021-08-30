67eb11289ba73c65cf7b14aa1a87136643d54cd8
Andika .R <andika15ramdani@gmail.com>
2021-08-30 18:43:49 +0700

    abc

---
+++ resources/views/admin/artikel-baru.blade.php
@@ -0,0 +1,69 @@
+<!-- Classic Modal -->
+<div class="modal fade" id="artikelBaru" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Tambah Anggota</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+          <i class="material-icons">clear</i>
+        </button>
+      </div>
+      <form action="{{ route('buatArtikel') }}" method="post" enctype="multipart/form-data" class="form-horizontal">
+        @csrf
+        <div class="modal-body">
+          <div class="row">
+            <label class="col-sm-4 col-form-label">Nama</label>
+            <div class="col-sm-8">
+              <div class="form-group">
+                <input type="text" name="nama" value="{{Session::get('name')}}" class="form-control" disabled>
+                <span class="bmd-help">Errors message goes here</span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <label class="col-sm-4 col-form-label">Judul</label>
+            <div class="col-sm-8">
+              <div class="form-group">
+                <input type="text" name="judul" class="form-control">
+                <span class="bmd-help">Errors message goes here</span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <label class="col-sm-4 col-form-label">Konten</label>
+            <div class="col-sm-8">
+              <div class="form-group">
+                <textarea name="deskripsi" id="" class="form-control"></textarea>
+                <span class="bmd-help">Errors message goes here</span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <label class="col-sm-4 col-form-label">Tanggal</label>
+            <div class="col-sm-8">
+              <div class="form-group">
+                <input type="date" name="tanggal" class="form-control">
+                <span class="bmd-help">Errors message goes here</span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <label class="col-sm-4 col-form-label">Gambar</label>
+            <div class="col-sm-8">
+              <div class="form-group">
+                <input type="file" name="gambar" class="form-control">
+                <input type="file" id="inputFile4" multiple="">
+                <span class="bmd-help">Errors message goes here</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <input type="submit" class="btn btn-success btn-link">
+          <input type="reset" class="btn btn-danger btn-link">
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<!--  End Modal -->
